da0ad93f1308aa82978718111b9cdc7669933c6b
MehmetGursul <mehmetgursul@gmail.com>
2019-11-19 23:54:20 +0100

    Counter component was added

---
+++ src/components/Counter.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Counter.css'
+
+class Counter extends Component {
+    render() {
+        return (
+            <div className="counter">
+                <button onClick={increment}>+1</button>
+                <span className="count">0</span>
+            </div>
+        );
+    }
+}
+
+function increment() {
+    console.log('increment');
+}
+
+export default Counter
