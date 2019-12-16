0677eb6e7af8f2a7298ed2aa04adab2806504e27
MehmetGursul <mehmetgursul@gmail.com>
2019-12-16 23:29:12 +0100

    Hardcoded Auth was added to the Login Component

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -13,10 +13,13 @@ class LoginComponent extends Component {
         super(props)
         this.state = {
             username: 'User',
-            password: ''
+            password: '',
+            hasLoginFailed: false,
+            showSuccessMessage: false
         }
 
         this.handleChange = this.handleChange.bind(this)
+        this.loginClicked = this.loginClicked.bind(this)
 
     }
 
@@ -29,15 +32,50 @@ class LoginComponent extends Component {
         )
     }
 
+    loginClicked() {
+
+        if (this.state.username === 'aaa' && this.state.password === 'bbb') {
+            console.log('Successfull')
+            this.setState({ showSuccessMessage: true })
+            this.setState({ hasLoginFailed: false })
+        } else {
+            console.log('Failed')
+            this.setState({ showSuccessMessage: false })
+            this.setState({ hasLoginFailed: true })
+        }
+
+
+    }
+
     render() {
         return (
             <div>
+                <ShowInvalidCredential hasLoginFailed={this.state.hasLoginFailed} />
+                <ShowLoginSuccessful showSuccessMessage={this.state.showSuccessMessage} />
                 User Name: <input type="text" name="username" ref={this.state.username} onChange={this.handleChange} defaultValue=""></input>
                 Password: <input type="password" name="password" ref={this.state.password} onChange={this.handleChange} defaultValue=""></input>
-                <button>Login</button>
+                <button onClick={this.loginClicked}>Login</button>
             </div>
         )
     }
+}
+
+function ShowInvalidCredential(props) {
+
+    if (props.hasLoginFailed) {
+        return <div>Invalid Credentials</div>
+    }
+
+    return null
+}
+
+function ShowLoginSuccessful(props) {
+
+    if (props.showSuccessMessage) {
+        return <div>Login Succesfull</div>
+    }
+
+    return null
 }
 
 export default TodoApp;
