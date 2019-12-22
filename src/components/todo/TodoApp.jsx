51f8a1029a9fb6c145153baa494bc8634a93da7f
MehmetGursul <mehmetgursul@gmail.com>
2019-12-22 21:28:56 +0100

    Styling components with CSS and Bootstrap

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -32,7 +32,14 @@ function ErrorComponent() {
 
 class WelcomeComponent extends Component {
     render() {
-        return <div> Welcome {this.props.match.params.name}. You can manage your todos <Link to="/todos"> here </Link> </div>
+        return (
+            <>
+                <h1>Welcome!</h1>
+                <div className="container">
+                    Welcome {this.props.match.params.name}.You can manage your todos < Link to="/todos" > here </Link >
+                </div>
+            </>
+        )
     }
 }
 
@@ -97,30 +104,31 @@ class ListTodosComponent extends Component {
         return (
             <div>
                 <h1>List Todos</h1>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>Description</th>
-                            <th>Done</th>
-                            <th>Target Date</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            this.state.todos.map(
-                                todo =>
-                                    <tr key={todo.id}>
-                                        <td>{todo.id}</td>
-                                        <td>{todo.description}</td>
-                                        <td>{todo.done.toString()}</td>
-                                        <td>{todo.targetDate.toString()}</td>
-                                    </tr>
-                            )
-                        }
 
-                    </tbody>
-                </table>
+                <div className="container">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>Description</th>
+                                <th>Done</th>
+                                <th>Target Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.todos.map(
+                                    todo =>
+                                        <tr key={todo.id}>
+                                            <td>{todo.description}</td>
+                                            <td>{todo.done.toString()}</td>
+                                            <td>{todo.targetDate.toString()}</td>
+                                        </tr>
+                                )
+                            }
+
+                        </tbody>
+                    </table>
+                </div>
             </div>
         )
     }
@@ -166,14 +174,14 @@ class LoginComponent extends Component {
     render() {
         return (
             <div>
-                {/*<ShowInvalidCredential hasLoginFailed={this.state.hasLoginFailed} /> */}
-                {this.state.hasLoginFailed && <div> Invalid Credentials </div>}
-
-                {/*<ShowLoginSuccessful showSuccessMessage={this.state.showSuccessMessage} /> */}
-                {this.state.showSuccessMessage && <div> Login Successfull </div>}
-                User Name: <input type="text" name="username" ref={this.state.username} onChange={this.handleChange} defaultValue=""></input>
-                Password: <input type="password" name="password" ref={this.state.password} onChange={this.handleChange} defaultValue=""></input>
-                <button onClick={this.loginClicked}>Login</button>
+                <h1>Login</h1>
+                <div className="container">
+                    {this.state.hasLoginFailed && <div className="alert alert-warning"> Invalid Credentials </div>}
+                    {this.state.showSuccessMessage && <div> Login Successfull </div>}
+                    User Name: <input type="text" name="username" ref={this.state.username} onChange={this.handleChange} defaultValue=""></input>
+                    Password: <input type="password" name="password" ref={this.state.password} onChange={this.handleChange} defaultValue=""></input>
+                    <button className="btn btn-success" onClick={this.loginClicked}>Login</button>
+                </div>
             </div>
         )
     }
