3955842d46f1e075af70ac902eb85b73ccda2ce7
MehmetGursul <mehmetgursul@gmail.com>
2020-02-03 23:17:47 +0100

    Todo Form was designed with moment, Formik and Bootstrap

--- src/components/todo/TodoComponent.jsx
+++ src/components/todo/TodoComponent.jsx
@@ -1,8 +1,43 @@
 import React, { Component } from 'react'
+import moment from 'moment'
+import { Formik, Form, Field } from 'formik'
 
 class TodoComponent extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            id: this.props.match.params.id,
+            desccription: 'Learn Forms',
+            targetDate: moment(new Date()).format('DD-MM-YYYY')
+        }
+    }
+
     render() {
-        return (<div>Todo Component for id - {this.props.match.params.id}</div>)
+        return (
+            <div>
+                <h1>Todo</h1>
+                <div className="container">
+                    <Formik>
+                        {
+                            (props) => (
+                                <Form>
+                                    <fieldset className="form-group">
+                                        <label>Description</label>
+                                        <Field className="form-control" type="text" name="description" />
+                                    </fieldset>
+                                    <fieldset className="form-group">
+                                        <label>Target Date</label>
+                                        <Field className="form-control" type="date" name="targetDate" />
+                                    </fieldset>
+                                    <button type="submit" className="btn btn-success">Save</button>
+                                </Form>
+                            )
+                        }
+                    </Formik>
+
+                </div>
+            </div>)
     }
 }
 
