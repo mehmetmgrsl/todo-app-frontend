cedcbbcb0eaa06145df448067528ccebaed262dc
MehmetGursul <mehmetgursul@gmail.com>
2020-02-08 01:32:36 +0100

    Todo Rest API was used

--- src/components/todo/TodoComponent.jsx
+++ src/components/todo/TodoComponent.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import moment from 'moment'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
+import TodoDataService from '../../api/todo/TodoDataService.js'
+import AuthenticationService from './AuthenticationService.js'
 
 class TodoComponent extends Component {
 
@@ -8,13 +10,23 @@ class TodoComponent extends Component {
         super(props)
         this.state = {
             id: this.props.match.params.id,
-            description: 'Learn Forms Now',
+            description: '',
             targetDate: moment(new Date()).format('YYYY-MM-DD')
         }
 
         this.onSubmit = this.onSubmit.bind(this)
         this.validate = this.validate.bind(this)
 
+    }
+
+    componentDidMount() {
+        let username = AuthenticationService.getLoggedInUsername()
+        TodoDataService.retrieveTodo(username, this.state.id)
+            .then(response => this.setState({
+                description: response.data.description,
+                targetDate: moment(response.data.targetDate).format('YYYY-MM-DD'),
+
+            }))
     }
 
     onSubmit(values) {
@@ -51,6 +63,7 @@ class TodoComponent extends Component {
                         onSubmit={this.onSubmit}
                         validateOnChange={false}
                         validateOnBlur={false}
+                        enableReinitialize={true}
                     >
                         {
                             (props) => (
